8fc8aa45aea492075d7c454fade5deb997045872
Nate Rupsis <nathaniel.rupsis@homeadvisor.com>
2020-07-06 08:13:42 -0600

    removed from top level

--- question_1/question_1_1.sql
+++
@@ -1,8 +0,0 @@
-INSERT OVERWRITE TABLE 
-ddb_cs598_question_1_1_local 
-select Origin, Dest, origin_total + dest_total as total 
-from ((select Origin, count(Origin) as origin_total from `cs598.cs598task1` Group by Origin) as a 
-JOIN 
-(select Dest, count(Dest) as dest_total from 
-`cs598.cs598task1` Group by Dest) as b
-ON a.Origin = b.Dest) order by total desc limit 10;
